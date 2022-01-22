915e016adb6d9e0793ae3f20cf6ccd9cbeaed13f
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2022-01-22 01:04:42 -0300

    Envio de primeira atividade

---
+++ Atividade01/Atividade01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Atividade01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello";
+            string b = "Students";
+            Console.WriteLine(a +" "+ b + "!");
+        }
+    }
+}
